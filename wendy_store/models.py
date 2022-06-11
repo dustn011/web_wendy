5bbd9751c94188242dae2df7796b0c9425054224
dustn011 <chi8850@naver.com>
2022-06-11 10:33:31 +0900

    Post 모델 생성

--- wendy_store/models.py
+++ wendy_store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # 제목(title)
+    title = models.CharField(max_length=30)
+    # 내용(content)
+    content = models.TextField()
+
+    # 작성일(created_at)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 수정일(updated_at)
+    updated_at = models.DateTimeField(auto_now=True)
+    # 작성자 정보(author): 아직 안만듬
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
